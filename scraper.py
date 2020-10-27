439354d91c967cfe06df85200af41c0e7bc2f3df
jason <jasonctle@gmail.com>
2020-10-27 15:06:55 -0700

    Added request package and imported request into project

---
+++ scraper.py
@@ -0,0 +1,9 @@
+import requests
+
+URL = "https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
+}
+
+page = requests.get(URL, headers=headers)
