6f300746411e020d0fe1ed5fd751ff7f21b38766
jason <jasonctle@gmail.com>
2020-10-29 12:50:38 -0700

    imported bs4 package and parsed info from html page

--- scraper.py
+++ scraper.py
@@ -1,9 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
 
-URL = "https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442"
+URL = "https://www.theragun.com/us/en-us/pro-us.html"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
 }
 
 page = requests.get(URL, headers=headers)
+
+print(page)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+print(soup)
+
